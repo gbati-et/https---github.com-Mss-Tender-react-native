e1ab21c5393c42273565f0af66450461c2a09dea
gbati-et <gelila.adugnaw@gmail.com>
2023-09-06 11:17:55 +0300

    changes made

---
+++ src/screens/index.js
@@ -0,0 +1,15 @@
+//home screens
+export {default as Alert} from './home/Alert';
+export {default as Categories} from './home/Categories';
+export {default as HomeScreen} from './home/HomeScreen';
+export {default as Profile} from './home/Profile';
+
+// Auth Screens
+export {default as Login} from './auth/Login';
+export {default as Signup} from './auth/Signup';
+export {default as Signup1} from './auth/Signup1';
+export {default as Signup2} from './auth/Signup2';
+export {default as Welcome} from './auth/Welcome';
+export {default as OTPVerification} from './auth/OTPVerification';
+export {default as ResetPassword} from './auth/ResetPassword';
+//export {default as S} from './auth/Signup';
